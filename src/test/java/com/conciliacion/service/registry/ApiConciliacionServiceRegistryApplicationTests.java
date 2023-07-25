63396369c00cf5adebf96c48bf08564baa6ebda2
jvargas <vjanethh@gmail.com>
2023-07-24 20:33:09 -0500

    final

---
+++ src/test/java/com/conciliacion/service/registry/ApiConciliacionServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conciliacion.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConciliacionServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
